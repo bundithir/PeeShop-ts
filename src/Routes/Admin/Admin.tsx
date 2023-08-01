16ed37a630aac190ea3f2ed0c84ab72f65a1dc77
Bundit Hirunwongsawang <bundit.hir@gmail.com>
2023-08-01 20:18:32 +0700

    check status user admin

--- src/Routes/Admin/Admin.tsx
+++ src/Routes/Admin/Admin.tsx
@@ -1,12 +1,20 @@
-import { useAppDispatch } from "../../Store/hook";
+import { useAppDispatch , useAppSelector } from "../../Store/hook";
 import { FetchAdminProducts } from "../../Store/admin/admin-reducer";
 import { useEffect } from 'react'
 import { AntDTable } from "./AntDTable";
+import { Selectuser } from "../../Store/user/user-selector";
+import { useNavigate } from "react-router-dom";
 
 const Admin = ()=>{
     const dispatch = useAppDispatch()
+    const user = useAppSelector(Selectuser)
+    const Navigate = useNavigate()
     const FetchProductAdmin = async()=> await dispatch(FetchAdminProducts())
     useEffect(()=> {
+        if(user?.status !== 'Admin'){
+            alert('You are not Admin')
+            Navigate('/')
+        }
         FetchProductAdmin()
     },[])
     return (
